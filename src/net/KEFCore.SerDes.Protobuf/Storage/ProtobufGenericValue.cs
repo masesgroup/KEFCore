c4716feafa2d57c481d7f0428b7f8d9ffdef06cc
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2023-10-25 16:36:31 +0200

    Added Protobuf project, missing tests

---
+++ src/net/KEFCore.SerDes.Protobuf/Storage/ProtobufGenericValue.cs
@@ -0,0 +1,106 @@
+﻿/*
+*  Copyright 2023 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+
+namespace MASES.EntityFrameworkCore.KNet.Serialization.Protobuf.Storage
+{
+    public sealed partial class GenericValue
+    {
+        /// <summary>
+        /// Initializer for <paramref name="input"/>
+        /// </summary>
+        /// <param name="input">The value to insert</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public GenericValue(object input)
+        {
+            if (input is null)
+            {
+                NullValue = Google.Protobuf.WellKnownTypes.NullValue.NullValue;
+            }
+            else if (input is bool boolVal)
+            {
+                BoolValue = boolVal;
+            }
+            else if (input is byte byteVal)
+            {
+                ByteValue = byteVal;
+            }
+            else if (input is short shortVal)
+            {
+                ShortValue = shortVal;
+            }
+            else if (input is int intVal)
+            {
+                IntValue = intVal;
+            }
+            else if (input is long longVal)
+            {
+                LongValue = longVal;
+            }
+            else if (input is float floatVal)
+            {
+                FloatValue = floatVal;
+            }
+            else if (input is double doubleVal)
+            {
+                DoubleValue = doubleVal;
+            }
+            else if (input is string stringVal)
+            {
+                StringValue = stringVal;
+            }
+            else if (input is Guid guidVal)
+            {
+                GuidValue = ByteString.CopyFrom(guidVal.ToByteArray());
+            }
+            else if (input is DateTime dateTimeVal)
+            {
+                DatetimeValue = Timestamp.FromDateTime(dateTimeVal);
+            }
+            else if (input is DateTimeOffset dateTimeOffsetVal)
+            {
+                DatetimeoffsetValue = Timestamp.FromDateTimeOffset(dateTimeOffsetVal);
+            }
+            else throw new InvalidOperationException($"{input.GetType()} is not managed.");
+        }
+        /// <summary>
+        /// Returns the content of <see cref="GenericValue"/>
+        /// </summary>
+        public object GetContent()
+        {
+            return KindCase switch
+            {
+                KindOneofCase.NullValue => null!,
+                KindOneofCase.BoolValue => BoolValue,
+                KindOneofCase.ByteValue => ByteValue,
+                KindOneofCase.ShortValue => ShortValue,
+                KindOneofCase.IntValue => IntValue,
+                KindOneofCase.LongValue => LongValue,
+                KindOneofCase.FloatValue => FloatValue,
+                KindOneofCase.DoubleValue => DoubleValue,
+                KindOneofCase.StringValue => StringValue,
+                KindOneofCase.GuidValue => new Guid(GuidValue.ToArray()),
+                KindOneofCase.DatetimeValue => DatetimeValue.ToDateTime(),
+                KindOneofCase.DatetimeoffsetValue => DatetimeValue.ToDateTimeOffset(),
+                _ => throw new InvalidOperationException($"{KindCase} is not managed."),
+            };
+        }
+    }
+}
---
+++ src/net/KEFCore.SerDes.Protobuf/Storage/ProtobufValueContainer.cs
@@ -0,0 +1,146 @@
+/*
+*  Copyright 2023 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+// #define DEBUG_PERFORMANCE
+
+#nullable enable
+
+using Google.Protobuf;
+using Google.Protobuf.Reflection;
+using Google.Protobuf.WellKnownTypes;
+
+namespace MASES.EntityFrameworkCore.KNet.Serialization.Protobuf.Storage;
+
+/// <summary>
+/// The default ValueContainer used from KEFCore
+/// </summary>
+/// <typeparam name="TKey">It is the key <see cref="Type"/> passed from Entity Framework associated to the Entity data will be stored in the <see cref="ProtobufValueContainer{TKey}"/></typeparam>
+public partial class ProtobufValueContainer<TKey> : IMessage<ProtobufValueContainer<TKey>>, IValueContainer<TKey>
+    where TKey : notnull
+{
+    readonly ProtobufValueContainer _innerMessage;
+
+    /// <summary>
+    /// Initialize a new instance of <see cref="ProtobufValueContainer{TKey}"/>
+    /// </summary>
+    /// <remarks>It is mainly used from the JSON serializer</remarks>
+    public ProtobufValueContainer() { _innerMessage = new ProtobufValueContainer(); _innerMessage.EntityName = _innerMessage.ClrType = null!; }
+    /// <summary>
+    /// Initialize a new instance of <see cref="ProtobufValueContainer{TKey}"/>
+    /// </summary>
+    /// <remarks>It is mainly used from the JSON serializer</remarks>
+    public ProtobufValueContainer(ProtobufValueContainer clone) { _innerMessage = clone; }
+    /// <summary>
+    /// Initialize a new instance of <see cref="ProtobufValueContainer{TKey}"/>
+    /// </summary>
+    /// <remarks>It is mainly used from the JSON serializer</remarks>
+    public ProtobufValueContainer(ProtobufValueContainer<TKey> clone) { _innerMessage = clone._innerMessage.Clone(); }
+    /// <summary>
+    /// Initialize a new instance of <see cref="ProtobufValueContainer{TKey}"/>
+    /// </summary>
+    /// <param name="tName">The <see cref="IEntityType"/> requesting the <see cref="ProtobufValueContainer{TKey}"/> for <paramref name="rData"/></param>
+    /// <param name="rData">The data, built from EFCore, to be stored in the <see cref="ProtobufValueContainer{TKey}"/></param>
+    /// <remarks>This constructor is mandatory and it is used from KEFCore to request a <see cref="ProtobufValueContainer{TKey}"/></remarks>
+    public ProtobufValueContainer(IEntityType tName, object[] rData)
+    {
+        _innerMessage = new ProtobufValueContainer
+        {
+            EntityName = tName.Name,
+            ClrType = tName.ClrType.FullName!
+        };
+        _innerMessage.Data.Clear();
+        foreach (var item in tName.GetProperties())
+        {
+            int index = item.GetIndex();
+            var pRecord = new PropertyDataRecord
+            {
+                PropertyIndex = index,
+                PropertyName = item.Name,
+                ClrType = item.ClrType?.FullName,
+                Value = new GenericValue(rData[index])
+            };
+            _innerMessage.Data.Add(pRecord);
+        }
+    }
+    /// <inheritdoc/>
+    public string EntityName => _innerMessage.EntityName;
+    /// <inheritdoc/>
+    public string ClrType => _innerMessage.ClrType;
+    /// <inheritdoc/>
+    public MessageDescriptor Descriptor => (_innerMessage as IMessage).Descriptor;
+    /// <inheritdoc/>
+    public int CalculateSize() => _innerMessage.CalculateSize();
+    /// <inheritdoc/>
+    public ProtobufValueContainer<TKey> Clone() => new ProtobufValueContainer<TKey>(this);
+    /// <inheritdoc/>
+    public bool Equals(ProtobufValueContainer<TKey>? other) => _innerMessage.Equals(other);
+    /// <inheritdoc/>
+    public void MergeFrom(ProtobufValueContainer<TKey> message) => _innerMessage.MergeFrom(message._innerMessage);
+    /// <inheritdoc/>
+    public void MergeFrom(CodedInputStream input) => _innerMessage.MergeFrom(input);
+    /// <inheritdoc/>
+    public void WriteTo(CodedOutputStream output) => _innerMessage.WriteTo(output);
+
+    /// <inheritdoc/>
+    public void GetData(IEntityType tName, ref object[] array)
+    {
+#if DEBUG_PERFORMANCE
+        Stopwatch fullSw = new Stopwatch();
+        Stopwatch newSw = new Stopwatch();
+        Stopwatch iterationSw = new Stopwatch();
+        try
+        {
+            fullSw.Start();
+#endif
+        if (_innerMessage.Data == null) { return; }
+#if DEBUG_PERFORMANCE
+            newSw.Start();
+#endif
+        array = new object[_innerMessage.Data.Count];
+#if DEBUG_PERFORMANCE
+            newSw.Stop();
+            iterationSw.Start();
+#endif
+        for (int i = 0; i < _innerMessage.Data.Count; i++)
+        {
+            array[i] = _innerMessage.Data[i].Value.GetContent();
+        }
+#if DEBUG_PERFORMANCE
+            iterationSw.Stop();
+            fullSw.Stop();
+        }
+        finally
+        {
+            if (Infrastructure.KafkaDbContext.TraceEntityTypeDataStorageGetData)
+            {
+                Infrastructure.KafkaDbContext.ReportString($"Time to GetData with length {Data.Count}: {fullSw.Elapsed} - new array took: {newSw.Elapsed} - Iteration took: {iterationSw.Elapsed}");
+            }
+        }
+#endif
+    }
+    /// <inheritdoc/>
+    public IReadOnlyDictionary<int, string> GetProperties()
+    {
+        Dictionary<int, string> props = new();
+        for (int i = 0; i < _innerMessage.Data.Count; i++)
+        {
+            props.Add(_innerMessage.Data[i].PropertyIndex, _innerMessage.Data[i].PropertyName);
+        }
+        return props;
+    }
+}
